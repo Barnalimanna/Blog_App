aac41a6e08f9c1ee93ec071de1fd6170b3f53b68
Barnali Manna <mannabarnali294@gmail.com>
2026-05-20 23:23:17 +0530

    fix forgot password api route

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -14,7 +14,7 @@ API.interceptors.request.use((config) => {
 
 export const registerUser = (data) => API.post('/auth/register', data);
 export const loginUser = (data) => API.post('/auth/login', data);
-export const forgotPassword = (email) => API.post('auth/forgot-password', { email });
+export const forgotPassword = (email) => API.post('/auth/forgot-password', { email });
 export const resetPassword = (token,password) => API.put(`/auth/reset-password/${token}`, { password });
 export const getProfile = () => API.get('/auth/profile');
 export const updateProfile = (data) => API.put('/auth/profile', data);
