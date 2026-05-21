7f3023be507d44d51e5325369037bc9bc229b46b
Barnali Manna <mannabarnali294@gmail.com>
2026-05-21 23:33:32 +0530

    switch email provider to brevo smtp

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,26 +1,50 @@
+// const nodemailer = require('nodemailer');
+
+// const sendEmail = async({ email, subject, message }) => {
+//     const transporter = nodemailer.createTransport({
+//         host: 'smtp.gmail.com',
+//         port: 587,
+//         secure: false,
+//         family: 4,
+//         auth:{
+//             user: process.env.EMAIL_USER,
+//             pass: process.env.EMAIL_PASS,
+//         },
+//         connectionTimeout: 20000,
+//         greetingTimeout: 20000,
+//         socketTimeout: 20000,
+//     });
+
+//     await transporter.sendMail({
+//         from: `"Post Nest" <${process.env.EMAIL_USER}>`,
+//         to: email,
+//         subject,
+//         html: message,
+//     });
+// };
+
+// module.exports = sendEmail;
+
+
 const nodemailer = require('nodemailer');
 
-const sendEmail = async({ email, subject, message }) => {
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        family: 4,
-        auth:{
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-        connectionTimeout: 20000,
-        greetingTimeout: 20000,
-        socketTimeout: 20000,
-    });
+const sendEmail = async ({ email, subject, message }) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
 
-    await transporter.sendMail({
-        from: `"Post Nest" <${process.env.EMAIL_USER}>`,
-        to: email,
-        subject,
-        html: message,
-    });
+  await transporter.sendMail({
+    from: `"Post Nest" <${process.env.SMTP_USER}>`,
+    to: email,
+    subject,
+    html: message,
+  });
 };
 
 module.exports = sendEmail;
