5030e2f25300580fb6888d61af002366b42f8d61
Barnali Manna <mannabarnali294@gmail.com>
2026-05-22 00:00:50 +0530

    Swith to brevo api

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -26,28 +26,64 @@
 // module.exports = sendEmail;
 
 
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
+
+// const sendEmail = async ({ email, subject, message }) => {
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.SMTP_HOST,
+//     port: Number(process.env.SMTP_PORT),
+//     secure: false,
+//     auth: {
+//       user: process.env.SMTP_USER,
+//       pass: process.env.SMTP_PASS,
+//     },
+//     connectionTimeout: 10000,
+//     greetingTimeout: 10000,
+//     socketTimeout: 10000,
+//   });
+
+//   await transporter.sendMail({
+//     from: `"Post Nest" <${process.env.SMTP_USER}>`,
+//     to: email,
+//     subject,
+//     html: message,
+//   });
+// };
+
+// module.exports = sendEmail;
+
+const axios = require('axios');
 
 const sendEmail = async ({ email, subject, message }) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: false,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
-  });
 
-  await transporter.sendMail({
-    from: `"Post Nest" <${process.env.SMTP_USER}>`,
-    to: email,
-    subject,
-    html: message,
-  });
+  await axios.post(
+    'https://api.brevo.com/v3/smtp/email',
+
+    {
+      sender: {
+        name: 'PostNest',
+        email: process.env.EMAIL_USER,
+      },
+
+      to: [
+        {
+          email: email,
+        },
+      ],
+
+      subject: subject,
+
+      htmlContent: message,
+    },
+
+    {
+      headers: {
+        accept: 'application/json',
+        'api-key': process.env.BREVO_API_KEY,
+        'content-type': 'application/json',
+      },
+    }
+  );
 };
 
 module.exports = sendEmail;
